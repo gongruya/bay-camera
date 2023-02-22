447fe215efc6b7509d9225edbecab48a1d7b2a49
Ruya Gong <gongruya@gmail.com>
2023-02-22 09:34:13 -0800

    Add salesforce tower webcams

---
+++ src/VideoPlayerWrapper.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function VideoPlayerWrapper({ children }) {
+  return (
+    <Box sx={{
+      position: 'relative',
+      paddingBottom: '56.25%',
+      'iframe': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+      }
+    }}>
+      {children}
+    </Box>
+  );
+};
--- src/YoutubeVideoPlayer.tsx
+++ src/YoutubeVideoPlayer.tsx
@@ -1,25 +1,15 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import VideoPlayerWrapper from './VideoPlayerWrapper.tsx';
 
 export default function YoutubeVideoPlayer({ title, videoId }) {
   return (
-    <Box sx={{
-      position: 'relative',
-      paddingBottom: '56.25%',
-      'iframe': {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        height: '100%',
-        width: '100%',
-      }
-    }}>
+    <VideoPlayerWrapper>
       <iframe src={'https://www.youtube-nocookie.com/embed/' + videoId}
         frameBorder="0"
         title={title}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen>
       </iframe>
-    </Box>
+    </VideoPlayerWrapper>
   );
 };
