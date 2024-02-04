af28440a30a8fcbb26dac4cdbb1cffbe91f4cc69
Ruya Gong <gongruya@gmail.com>
2024-02-03 23:54:23 -0500

    Add logo and loading status for cloud map

--- src/app/cloud/CloudPageWrapper.tsx
+++ src/app/cloud/CloudPageWrapper.tsx
@@ -2,11 +2,14 @@
 
 import moment from 'moment';
 import {CloudCoverage, CloudLevel, fetchHrrrCloud, hrrrRange} from '@/weather/hrrr';
-import {Box, Button, Drawer, FormControl, IconButton, InputLabel, MenuItem, Select, Slider, Snackbar} from '@mui/material';
+import {Box, Button, CircularProgress, Drawer, FormControl, IconButton, InputLabel, MenuItem, Select, Slider, Snackbar, Typography} from '@mui/material';
 import dynamic from 'next/dynamic';
 import {useEffect, useState} from 'react';
 import {LatLngBounds} from 'leaflet';
 import CloseIcon from '@mui/icons-material/Close';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import CloudDoneIcon from '@mui/icons-material/CloudDone';
+import CloudOffIcon from '@mui/icons-material/CloudOff';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const LeafletMapContainer =
@@ -21,6 +24,8 @@ export function CloudPageWrapper() {
   const [cloudMap, setCloudMap] = useState<CloudCoverage[]>([]);
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [errorOpen, setErrorOpen] = useState(false);
+  const [errorFound, setErrorFound] = useState(false);
+  const [loading, setLoading] = useState(0);
 
   useEffect(() => {
     const date = moment().subtract(1, 'hour').startOf('hour').toDate();
@@ -30,16 +35,46 @@ export function CloudPageWrapper() {
 
   useEffect(() => {
     if (modelDate && bounds) {
+      setLoading((l) => l + 1);
       fetchHrrrCloud(modelDate, forecastHours, cloudLevel, bounds)
         .then(({cloud}) => {
           setCloudMap(cloud || []);
+          setErrorFound(false);
         }).catch(() => {
           setErrorOpen(true);
+          setErrorFound(true);
+        }).finally(() => {
+          setLoading((l) => l - 1);
         });
     }
   }, [modelDate, forecastHours, cloudLevel]);
 
   return (modelDate && currentDate && <>
+    <Typography variant='h5' color='primary' position='absolute' display='flex'
+      sx={{
+        pt: 2,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+      {loading > 0 &&
+        <Box display='inline-block' position='relative' sx={{width: 32, height: 32}}>
+          <CircularProgress size={32} sx={{position: 'absolute', top: 2}} />
+          <Box position='absolute'><CloudDownloadIcon sx={{m: 0.5}} /></Box>
+        </Box>
+      }
+      {loading == 0 &&
+        <Box display='inline-block' sx={{pl: '4px', width: 32, height: 32}} >
+          {!errorFound && <CloudDoneIcon />}
+          {errorFound && <CloudOffIcon color='error' />}
+        </Box>
+      }
+      <Typography variant='h5' component='div' display='inline-block'
+        sx={{ml: 1}} color={errorFound ? 'error' : 'inherit'}
+      >
+        bay.camera
+      </Typography>
+    </Typography>
     <LeafletMapContainer style={{
       position: 'absolute',
       top: 0,
@@ -52,11 +87,16 @@ export function CloudPageWrapper() {
       onChange={async (bounds) => {
         setBounds(bounds);
         try {
+          setLoading((l) => l + 1);
           const {cloud} =
             await fetchHrrrCloud(modelDate, forecastHours, cloudLevel, bounds);
           setCloudMap(cloud || []);
+          setErrorFound(false);
         } catch {
           setErrorOpen(true);
+          setErrorFound(true);
+        } finally {
+          setLoading((l) => l - 1);
         }
       }}
     />
