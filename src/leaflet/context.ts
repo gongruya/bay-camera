34cf05ab0685f1b27185645a955e011821c36ce7
Ruya Gong <gongruya@gmail.com>
2024-02-17 02:02:04 -0500

    Clean up leaflet react context

--- src/leaflet/context.ts
+++ src/leaflet/context.ts
@@ -4,6 +4,5 @@ import L from 'leaflet';
 export const LeafletContext = createContext<L.Map | undefined>(undefined);
 
 export function useLeafletMap(): L.Map | undefined {
-  const map = useContext(LeafletContext);
-  return map;
+  return useContext(LeafletContext);
 }
