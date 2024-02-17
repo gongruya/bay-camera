539f5deb9967df77ede56b76425c0cf5cbca132e
Ruya Gong <gongruya@gmail.com>
2024-02-17 03:08:52 -0500

    Fix slider

--- src/app/cloud/CloudPageWrapper.tsx
+++ src/app/cloud/CloudPageWrapper.tsx
@@ -208,7 +208,7 @@ export function CloudPageWrapper() {
     }}>
       <Slider
         aria-label='Forecast time'
-        value={forecastHours}
+        defaultValue={2}
         valueLabelFormat={
           (h) => {
             const m = moment(modelDate).add(h, 'hour');
